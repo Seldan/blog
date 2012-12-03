f074c26c7f39921cc75569505bf0c412e8e61d94
Seldan <spam@seldan.de>
2012-12-03 21:49:34 +0100

    control panel major overhaul

--- admin.php
+++ admin.php
@@ -1,4 +1,17 @@
 <?php
+if(!empty($_GET['action'])) {
+    $action = $_GET['action'];
+    /*redirect to mainpage on logout*/
+    if ($action == "logout") {
+        session_start();
+        session_destroy();
+        require "conf/main.conf.php";
+        header ('HTTP/1.1 307 Temporary Redirect');
+        header ('Location: '.$site_url.'/index.php');
+    }
+} else {
+    $action = 'stats';
+}
 session_start();
 ?>
 <!DOCTYPE html>
@@ -12,16 +25,27 @@ session_start();
 </head>
 <body>
     <div id="container">
-        <?php include "header.php"; ?>
-        <?php
-if (!empty($_SESSION["admin"])){
-        echo "<div id=\"bar\"><a href=?action=post>post</a> | <a href=?action=stats>stats</a> | <a href=?action=debug>debug</a> | <a href=?action=logout>logout</a></div>";
+        <?php include "header.php"; 
+        echo "<div id=\"bar\"><a href=?action=post>post</a> | <a href=?action=stats>stats</a> | <a href=?action=debug>debug</a></div>";
         echo "\n<br />";
-        if(!empty($_GET['action'])) {
-            $action = $_GET['action'];
-        } else {
-            $action = 'stats';
+if (empty($_SESSION["admin"])){ //not authenticated, ask for password
+    if(isset($_POST["password"])) { //password typed in, check it
+        require "conf/main.conf.php";
+        $input_pw = $_POST["password"];
+        //hash password
+        for ($i = 0; $i < 65536; $i++) {
+            $input_pw = hash('md5', $input_pw);
         }
+        if($input_pw == $password) { //grant access
+            $_SESSION["admin"] = 1;
+        } else { //wrong password
+            echo "access denied!";
+        }
+    } else { //show login form
+        echo "<form method=\"post\"><div style=\"text-align: center;\" class=\"input-append\"><input type=password name=\"password\" placeholder=\"Password\" size=20 /> <input name=\"login\" type=\"submit\" class=\"btn btn-inverse\" /></div></form>";
+    }
+}
+if (!empty($_SESSION["admin"])) {
     /*all actions here should be selfexplaining*/
     switch ($action) {
         case 'post':
@@ -38,33 +62,14 @@ if (!empty($_SESSION["admin"])){
             $kill = "yes";
             require "stats.php";
             break;
-        case 'logout':
-            session_destroy();
-            echo "logout successful! <a href=admin.php>Click here to leave the Control Panel</a>.";
-            break;
         default:
             echo "Sorry, there is such action!";
             break;
     } 
-} else { //not authenticated, ask for password
-    if(isset($_POST["password"])) { //password typed in, check it
-        require "conf/main.conf.php";
-        $input_pw = $_POST["password"];
-        //hash password
-        for ($i = 0; $i < 65536; $i++) {
-            $input_pw = hash('md5', $input_pw);
-        }
-        if($input_pw == $password) { //grant access
-            $_SESSION["admin"] = 1;
-            echo "login successful! <a href=admin.php>Click here to enter the Control Panel</a>.";
-        } else { //wrong password
-            echo "access denied!";
-        }
-    } else { //show login form
-        echo "Password: <form method=\"post\"><input type=password name=\"password\" /> <input type=submit />";
-    }
+    echo '<a style="float: right;" href=?action=logout>logout</a>';
 }
         ?>
+
     </div>
 </body>
 </html>
