6b6d5b4e748b5e758b8f6ca6adc3d595dfb8fca5
Seldan <register@seldan.de>
2013-02-11 12:00:52 +0100

    ' is now used instead of a wild mix of ' and "

--- admin.php
+++ admin.php
@@ -2,10 +2,10 @@
 if(!empty($_GET['action'])) {
     $action = $_GET['action'];
     /*redirect to mainpage on logout*/
-    if ($action == "logout") {
+    if ($action == 'logout') {
         session_start();
         session_destroy();
-        require "conf/main.conf.php";
+        require 'conf/main.conf.php';
         header ('HTTP/1.1 307 Temporary Redirect');
         header ('Location: '.$site_url.'/index.php');
     }
@@ -25,53 +25,54 @@ session_start();
 </head>
 <body>
     <div id="container">
-        <?php include "header.php"; 
+        <?php 
+        include 'header.php'; 
         echo "<div id=\"bar\"><a href='?action=post'>post</a> | <a href='?action=managecomments'>comments</a> | <a href='?action=manageposts'>posts</a> | <a href='?action=stats'>stats</a></div>";
         echo "\n<br />";
-if (empty($_SESSION["admin"])){ //not authenticated, ask for password
-    if(isset($_POST["password"])) { //password typed in, check it
-        require "conf/main.conf.php";
-        $input_pw = $_POST["password"];
+if (empty($_SESSION['admin'])){ //not authenticated, ask for password
+    if(isset($_POST['password'])) { //password typed in, check it
+        require 'conf/main.conf.php';
+        $input_pw = $_POST['password'];
         //hash password
         for ($i = 0; $i < 65536; $i++) {
             $input_pw = hash('md5', $input_pw);
         }
         if($input_pw == $password) { //grant access
-            $_SESSION["admin"] = 1;
+            $_SESSION['admin'] = 1;
         } else { //wrong password
-            echo "access denied!";
+            echo 'Access denied!';
         }
     } else { //show login form
-        echo "<form method=\"post\"><div style=\"text-align: center;\" class=\"input-append\"><input type=password name=\"password\" placeholder=\"Password\" size=20 /> <input name=\"login\" type=\"submit\" class=\"btn btn-inverse\" /></div></form>";
+        echo '<form method="post"><div style="text-align: center;" class="input-append"><input type=password name="password" placeholder="Password" size=20 /> <input name="login" type="submit" class="btn btn-inverse" /></div></form>';
     }
 }
-if (!empty($_SESSION["admin"])) {
+if (!empty($_SESSION['admin'])) {
     /*all actions here should be selfexplaining*/
     switch ($action) {
         case 'post':
-            require "post.php";
+            require 'post.php';
             break;
         case 'stats':
-            require "stats.php";
+            require 'stats.php';
             break;
         case 'debug':
-            require "debug.php";
+            require 'debug.php';
             break;
         case 'removedeadcomments':
-            $kill = "yes";
-            require "stats.php";
+            $kill = 'yes';
+            require 'stats.php';
             break;
         case 'managecomments':
-            require "managecomments.php";
+            require 'managecomments.php';
             break;
         case 'manageposts':
-            require "manageposts.php";
+            require 'manageposts.php';
             break;
         default:
-            echo "Sorry, there is such action!";
+            echo 'Sorry, there is such action!';
             break;
     } 
-    echo '<a style="float: right;" href=\'?action=logout\'>logout</a>';
+    echo '<a style="float: right;" href="?action=logout">logout</a>';
 }
         ?>
 
