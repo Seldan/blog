ee5ba85cf79376fb784d6584b757f0b4ce2ba0b1
Seldan <spam@seldan.de>
2013-02-16 01:15:47 +0100

    Header links back to index.

--- header.php
+++ header.php
@@ -2,6 +2,6 @@
     if ( basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"]) ) { exit(); }
     require "conf/main.conf.php";
     if($header_show) {
-        echo "      <div id=\"header\"><img src=\"".$header_url."\" alt=\"Website header\"></div>\n";
+        echo "      <div id=\"header\"><a href=\"".$site_url."\" ><img src=\"".$header_url."\" alt=\"Website header\"></a></div>\n";
     }
 ?>
